258ced13f2c3ab8aaf8884f9bfe8df1ac9d6aa00
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2021-04-08 15:07:59 +0200

    :memo: POC : Mini server HTTP
    commit 5665d77c1457652302403370a888abab30be0e65
    Author: Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
    Date:   Thu Apr 8 15:04:29 2021 +0200
    
        :memo: POC : Add Mini-serv
    
    commit e190626293c252c15fcb4e2ee0759015176b4e7b
    Merge: df8141b 55b1b31
    Author: Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
    Date:   Thu Apr 8 15:02:49 2021 +0200
    
        Merge remote-tracking branch 'origin/main' into dev_jex
    
    commit df8141bb64a0e1b8c6617e515b5bfe7fa79dc37e
    Author: Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
    Date:   Thu Apr 8 11:57:53 2021 +0200
    
        :arrow_up: feat : POC Mini client HTTP
    
        - Finish POC Mini-client HTTP
    
    commit 2ac4b88fcced14c72bdaa6aa01489030980befa5
    Author: Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
    Date:   Wed Apr 7 17:36:53 2021 +0200
    
        :arrow_up: feat : Start POC Mini client HTTP
    
        - Add Makefile
        - Add file mini_client.cpp

---
+++ docs/poc/mini_serv/jx/mini_serv.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_serv.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jereligi <jereligi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/08 14:06:59 by jereligi          #+#    #+#             */
+/*   Updated: 2021/04/08 14:07:05 by jereligi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_SERV
+# define MINI_SERV
+
+# include <iostream>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <string.h>
+# include <unistd.h>
+# include "./GNL/get_next_line.h"
+
+#endif 
