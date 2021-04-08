073d32ad3554d75b94b6bcd22352948dd70dd859
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2021-04-08 17:59:32 +0200

    :memo: POC : Select first version

---
+++ docs/poc/select/jereligi/select.hpp
@@ -0,0 +1,13 @@
+
+#ifndef SELECT_HPP
+# define SELECT_HPP
+
+# include <sys/select.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <string.h>
+# include <unistd.h>
+# include <iostream>
+#include <fcntl.h>
+
+#endif
