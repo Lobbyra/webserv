15f825ef2d5ab7bf0e0d6ffa5dda1ed8ad611c42
Lobbyra <jecaudal@student.42.fr>
2021-04-30 16:04:48 +0200

    Improve terminal debug on s_socket and s_request_header

--- srcs/data_structures/s_request_header.hpp
+++ srcs/data_structures/s_request_header.hpp
@@ -5,20 +5,22 @@
 # include <string>
 
 struct s_request_header {
-        std::string                 method;
-        std::string                 path;
-        std::string                 protocol;
-        std::string                 date;
-        std::string                 host;
-        std::string                 referer;
-        std::string                 transfer_encoding;
-        std::list<std::string>      accept_charset;
-        std::list<std::string>      accept_language;
-        std::list<std::string>      authorization;
-        std::list<std::string>      content_type;
-        std::list<std::string>      user_agent;
-        size_t                      content_length;
-        size_t                      error;
+    std::string                 method;
+    std::string                 path;
+    std::string                 protocol;
+    std::string                 date;
+    std::string                 host;
+    std::string                 referer;
+    std::string                 transfer_encoding;
+    std::list<std::string>      accept_charset;
+    std::list<std::string>      accept_language;
+    std::list<std::string>      authorization;
+    std::list<std::string>      content_type;
+    std::list<std::string>      user_agent;
+    size_t                      content_length;
+    size_t                      error;
 };
+
+std::ostream& operator<<(std::ostream& os, s_request_header const &src);
 
 #endif
