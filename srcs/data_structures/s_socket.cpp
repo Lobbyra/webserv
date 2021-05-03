8f56bd6f315a0ab3fe68ce06ddd33dd7bf3ece64
mli <mli@student.42.fr>
2021-05-03 15:25:30 +0200

    :wrench: : read_request_header: don't check errno after gnl
    + s_socket print colored bool

--- srcs/data_structures/s_socket.cpp
+++ srcs/data_structures/s_socket.cpp
@@ -2,15 +2,20 @@
 #include "colors.hpp"
 
 std::ostream    &operator<<(std::ostream &o, s_socket const &i) {
-    o << COLOR_WHITE_("{entry_socket") " = " << \
-        i.entry_socket;
-    o << COLOR_WHITE_(", client_fd") " = " <<    \
-        i.client_fd;
-    o << COLOR_WHITE_(", is_read_ready") " = " << std::boolalpha << \
-        i.is_read_ready;
-    o << COLOR_WHITE_(", is_write_ready") " = " << std::boolalpha << \
-        i.is_write_ready;
-    o << COLOR_WHITE_(", is_header_read") " = " << std::boolalpha << \
-        i.is_header_read << "}" << std::endl;
+    o << "{";
+
+    o << COLOR_WHITE_("entry_socket") << " = " << i.entry_socket;
+    o << COLOR_WHITE_(", client_fd") << " = " << i.client_fd;
+    o << COLOR_WHITE_(", is_read_ready") << " = " << \
+        (i.is_read_ready ? COLOR_GREEN : COLOR_RED) << \
+        std::boolalpha << i.is_read_ready << COLOR_RESET;
+    o << COLOR_WHITE_(", is_write_ready") << " = " << \
+        (i.is_write_ready ? COLOR_GREEN : COLOR_RED) << \
+        std::boolalpha << i.is_write_ready << COLOR_RESET;
+    o << COLOR_WHITE_(", is_header_read") << " = " << \
+        (i.is_header_read ? COLOR_GREEN : COLOR_RED) << \
+        std::boolalpha << i.is_header_read << COLOR_RESET;
+
+    o << "}" << std::endl;
     return (o);
 }
