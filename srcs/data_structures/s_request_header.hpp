508db077bd34840cdaab6141353d983b20fe1576
Jérémy <jecaudal@student.42.fr>
2021-05-04 11:42:34 +0200

    add ignored headers saving and refactor read_request_header

--- srcs/data_structures/s_request_header.hpp
+++ srcs/data_structures/s_request_header.hpp
@@ -5,20 +5,21 @@
 # include <string>
 
 struct s_request_header {
-    std::string                 method;
-    std::string                 path;
-    std::string                 protocol;
-    std::string                 date;
-    std::string                 host;
-    std::string                 referer;
-    std::string                 transfer_encoding;
-    std::list<std::string>      accept_charset;
-    std::list<std::string>      accept_language;
-    std::list<std::string>      authorization;
-    std::list<std::string>      content_type;
-    std::list<std::string>      user_agent;
-    size_t                      content_length;
-    size_t                      error;
+    size_t                 error; // var that will be status-code
+    size_t                 content_length;
+    std::string            method;
+    std::string            path;
+    std::string            protocol;
+    std::string            date;
+    std::string            host;
+    std::string            referer;
+    std::string            transfer_encoding;
+    std::list<std::string> accept_charset;
+    std::list<std::string> accept_language;
+    std::list<std::string> authorization;
+    std::list<std::string> content_type;
+    std::list<std::string> user_agent;
+    std::list<std::string> ignored_headers;
 };
 
 std::ostream& operator<<(std::ostream& os, s_request_header const &src);
--- srcs/parse_request_header/parse_request_line.cpp
+++ srcs/parse_request_header/parse_request_line.cpp
@@ -1,35 +1,38 @@
 #include "parse_request.hpp"
 
-static size_t     check_request_line(std::map<std::string,
-                                     void *> request_header) {
-    if (((static_cast<std::string *>(request_header["Method"]))->length()) <= 0)
-        return (400);
-    else if (((static_cast<std::string *>(request_header["Path"]))->length()) <= 0)
-        return (400);
-    else if (((static_cast<std::string *>
-    (request_header["Protocol"]))->length()) <= 0)
-        return (400);
-    return (0);
+static void     check_request_line(std::map<std::string,void *> req_header) {
+    size_t *error_code = static_cast<size_t *>(req_header["Error"]);
+    std::string *method = static_cast<std::string *>(req_header["Method"]);
+    std::string *path = static_cast<std::string *>(req_header["Path"]);
+    std::string *protocol = static_cast<std::string *>(req_header["Protocol"]);
+
+    if (method->empty() == true) {
+        *error_code = 400;
+    } else if (path->empty() == true) {
+        *error_code = 400;
+    } else if (protocol->empty() == true) {
+        *error_code = 400;
+    }
 }
 
 static void    parse_protocol(std::string const &line,
                        std::string::const_iterator &it,
                        std::map<std::string, void *> request_header) {
     std::string     protocol;
-    std::string     sep(" ");   
+    std::string     sep(" ");
 
     protocol =  get_word(line, it, sep);
     std::string * ptr = static_cast<std::string *>(request_header["Protocol"]);
     *ptr = protocol;
 }
 
-static void    parse_path(std::string line, 
+static void    parse_path(std::string line,
                    std::map<std::string, void *> request_header) {
     std::string     path;
     std::string     sep(" ");
     std::size_t     found = line.find(sep);
     std::string::const_iterator it, ite;
-    
+
     if (found == std::string::npos)
         return ;
     it = line.begin() + found;
@@ -48,24 +51,23 @@ static void    parse_path(std::string line,
     parse_protocol(line, it, request_header);
 }
 
-void    parse_method(std::string line, 
+void    parse_method(std::string line,
                      std::map<std::string, void *> request_header) {
     std::string             prefix;
     std::string             sep(" ");
     std::list<std::string>  lst_prefix = init_prefix_method();
-    std::list<std::string>::iterator it, ite;
+    std::list<std::string>::iterator it = lst_prefix.begin();
+    std::list<std::string>::iterator ite = lst_prefix.end();
 
-    prefix =  get_word(line, line.begin(), sep);
-    for (it = lst_prefix.begin(); it != ite; it++)
-    {
-        if (prefix == *it)
-        {
+    prefix = get_word(line, line.begin(), sep);
+    while (it != ite) {
+        if (prefix == *it) {
             std::string * ptr = static_cast<std::string *>(request_header["Method"]);
             *ptr = prefix;
             parse_path(line, request_header);
             break ;
         }
+        ++it;
     }
-    size_t * ptr = static_cast<size_t *>(request_header["Error"]);
-    *ptr = check_request_line(request_header);
+    check_request_line(request_header);
 }
