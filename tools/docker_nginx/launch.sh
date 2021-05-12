2c3c6db8daa0a490b5bb4acc7ebca184885777aa
Marc <marc.li5101@gmail.com>
2021-05-12 14:58:46 +0200

    Finish status-line test on get/head (except on empty root)

--- tools/docker_nginx/launch.sh
+++ tools/docker_nginx/launch.sh
@@ -75,7 +75,7 @@ fi
 # ============================================================================ #
 
 if [ -z "$1" ]; then
-    1="./config_files/ok_basic.conf"
+    1="config_files/ok_basic_put.conf"
 fi
 
 if [ -f $1 ]; then
