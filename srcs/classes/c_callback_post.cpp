3a584a90b4a6f783ac91aa1705bd49e5eb1b5a5b
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2021-05-20 17:27:04 +0200

    :bug: fix : bug with our tester, free duplicate problem

--- srcs/classes/c_callback_post.cpp
+++ srcs/classes/c_callback_post.cpp
@@ -2,11 +2,12 @@
 
 void                        c_callback::_check_is_body_to_large(void) {
     struct stat     stat;
-
+    
     if (lstat(_tmpfile->get_filename().c_str(), &stat) != -1) {
             if (this->client_max_body_size != -1 && 
-                    stat.st_size > client_max_body_size)
+                    stat.st_size > client_max_body_size) {
                 this->status_code = 413;
+                }
     }
 }
 
@@ -15,17 +16,16 @@ void                        c_callback::_create_tmp_file(void) {
     char            *tmp;
     int             status;
 
-    if (this->client_max_body_size == -1)
-        return ;
-    if (*this->is_read_ready == false)
-        --_it_recipes;
     if (_tmpfile == NULL)
         _tmpfile = new c_tmpfile();
+    if (*this->is_read_ready == false)
+        --_it_recipes;
     if ((status = get_next(client_fd, &line, "\r\n")) == 1) {
         tmp = ft_strjoin(line, "\r\n");
         write(_tmpfile->get_fd(), tmp, ft_strlen(tmp));
         free(tmp);
         free(line);
+        line = NULL;
         --_it_recipes;
     }
     if (line != NULL)
@@ -40,8 +40,8 @@ std::list<c_callback::t_task_f>     c_callback::_init_recipe_post(void) {
         tasks.push_back(&c_callback::_chunk_reading_chunk);
     } else {
         tasks.push_back(&c_callback::_create_tmp_file);
+        tasks.push_back(&c_callback::_check_is_body_to_large);
     }
-    tasks.push_back(&c_callback::_check_is_body_to_large);
     tasks.push_back(&c_callback::_gen_resp_headers);
     tasks.push_back(&c_callback::_send_respons);
     return tasks;
