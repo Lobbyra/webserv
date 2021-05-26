a7e74d83229387ad5c83667fab8ba6aa8106bd43
mli42 <mli@student.42.fr>
2021-05-26 11:30:39 +0200

    :arrow_up: un-tar macos php-cgi

--- .prerequisite.sh
+++ .prerequisite.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
 CONFIG_PATH=./config_files
+PHPCGI_TARGET=./tools/php-cgi/
+PHPCGI_RES=php-8.0.2
 
 if [ "$OSTYPE" = "linux-gnu" ]; then
 	function sedreplace () {
 		sed s/$1/$2/g $3 -i
 	}
+	PHPCGI_SRC="./tools/.compressed_phpcgi/linux_phpcgi.tar"
 else
 	function sedreplace () {
 		sed -i "" s/$1/$2/g $3
 	}
+	PHPCGI_SRC="./tools/.compressed_phpcgi/darwin_phpcgi.tar"
 fi
 
+# un-tar phpcgi
+# Archive created by: tar -czf .compressed_phpcgi/darwin_phpcgi.tar php-8.0.2
+if ! [ -e "$PHPCGI_TARGET" ]; then
+	echo "Unpacking php-cgi ..."
+	tar -xzf $PHPCGI_SRC
+	mv $PHPCGI_RES $PHPCGI_TARGET
+fi
+
+# Replace __PWD__ by actual PWD in every config files
 FILES=($(ls $CONFIG_PATH))
 PWD=$(echo $PWD | sed s/\\//\\\\\\//g)
 for file in ${FILES[@]}; do
