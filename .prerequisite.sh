3c978f9b18ccce50749e34589da3670592c402fb
mli42 <mli@student.42.fr>
2021-05-28 12:25:28 +0200

    make: siege keep-alive on macOS

--- .prerequisite.sh
+++ .prerequisite.sh
@@ -4,6 +4,7 @@ CONFIG_PATH=./config_files
 PHPCGI_TARGET=tools/php-cgi
 PHPCGI_RES=php-8.0.2
 PHPBIN_PATH="$PHPCGI_TARGET/sapi/cgi/php-cgi"
+SIEGE_CONF="$HOME/.siege/siege.conf"
 
 function ft_escape_backslash () {
 	echo $* | sed s/\\//\\\\\\//g
@@ -11,14 +12,17 @@ function ft_escape_backslash () {
 
 if [ "$OSTYPE" = "linux-gnu" ]; then
 	function sedreplace () {
-		sed s/$1/$2/g $3 -i
+		sed s/"$1"/"$2"/g $3 -i
 	}
 	PHPCGI_SRC="./tools/.compressed_phpcgi/linux_phpcgi.tar"
-else
+else # On macOS
 	function sedreplace () {
-		sed -i "" s/$1/$2/g $3
+		sed -i "" s/"$1"/"$2"/g $3
 	}
 	PHPCGI_SRC="./tools/.compressed_phpcgi/darwin_phpcgi.tar"
+	if [ -e $SIEGE_CONF ]; then
+		sedreplace "^connection \= close" "connection \= keep-alive" $SIEGE_CONF
+	fi
 fi
 
 # un-tar phpcgi
