438b92193cc79cb7804d0fbb6f97705068020325
Lobbyra <jecaudal@student.42.fr>
2021-05-29 13:37:35 +0200

    :zap: opti : optimize GET cache +-1700/sec faster

--- srcs/similar_get_req.cpp
+++ srcs/similar_get_req.cpp
@@ -1,49 +1,40 @@
 #include "webserv.hpp"
 
-static bool    check_old_respons_is_valid(s_socket client,
-                    struct s_similar_get_req *similar_req) {
-    struct stat         stat;
+static bool    check_old_respons_is_valid(s_socket &client,
+                                          s_similar_get_req *similar_req) {
+    struct stat stat;
 
     errno = 0;
-    if (lstat(similar_req->path_respons.c_str(), &stat) == -1) {
-        std::cerr <<                                                          \
-            "Error: lstat check_old_respons_is_valid() : " << strerror(errno) \
-        << std::endl;
-        return (false);
-    }
-    if (similar_req->last_state_change != stat.st_ctime)
-        return (false);
-    if (ft_strcmp(client.headers.host.c_str(), similar_req->host.c_str()) != 0)
-        return (false);
     if (client.ipport != similar_req->ipport)
+        return (false);
+    if (client.headers.host != similar_req->host)
+        return (false);
+    if (similar_req->last_state_change != stat.st_ctime)
         return (false);
     return (true);
 }
 
-static void     send_same_request(s_socket *client,
-                                  s_similar_get_req *similar_req) {
-    if (send(client->client_fd, similar_req->respons.c_str(),
-        similar_req->respons.length(), 0) < 1) {
-        std::cerr << "Error: Respons to client" << std::endl;
-    }
-    reset_socket(client);
-}
-
 void    similar_get_req_manager(t_socketlst *clients,
                                 s_similar_get_req *similar_req) {
-    t_socketlst::iterator       it;
-    t_socketlst::iterator       ite;
+    struct stat                 stat;
+    t_socketlst::iterator       it = clients->begin();
+    t_socketlst::iterator       ite = clients->end();
 
-    it = clients->begin();
-    ite = clients->end();
     for (; it != ite; ++it) {
-        if ((*it).is_callback_created == false &&
-            (*it).is_write_ready == true &&
-            ft_strcmp((*it).headers.method.c_str(), "GET") == 0 &&
-            ft_strcmp((*it).headers.path.c_str(), "/") == 0 &&
-            ((*it).headers.error / 100 == 2)) {
-                if (check_old_respons_is_valid((*it), similar_req) == true)
-                    send_same_request(&(*it), similar_req);
+        if (lstat(similar_req->path_respons.c_str(), &stat) == -1)
+            continue ;
+        if (it->is_callback_created == false && it->is_write_ready == true &&
+                it->headers.method == "GET" && it->headers.path == "/"     &&
+                it->headers.error == 200                                   &&
+                check_old_respons_is_valid((*it), similar_req) == true     &&
+                it->ipport == similar_req->ipport                          &&
+                it->headers.host == similar_req->host                      &&
+                similar_req->last_state_change == stat.st_ctime) {
+            if (send(it->client_fd, similar_req->respons.c_str(),
+                        similar_req->respons.length(), 0) < 1) {
+                std::cerr << "Error: Respons to client" << std::endl;
+            }
+            reset_socket(&(*it));
         }
     }
 }
